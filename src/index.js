223234a1ca228db633bc0731b9de7e27e436d5bf
raprincis <princis.rakotomanga.pro@gmail.com>
2020-08-28 12:03:36 +0200

    express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.json({
+        message : 'Hello World'
+    })
+})
+
+app.listen(port, () => {
+    console.log('App running on ' + port)
+})
