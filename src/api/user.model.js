4156aa0160d01dce59dd5c5a08cae747fc09be19
raprincis <princis.rakotomanga.pro@gmail.com>
2020-08-28 23:35:32 +0200

    added db

---
+++ src/api/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    firstname : {
+        type: String,
+        required : true
+    },
+    lastname : {
+        type: String,
+        required : true
+    }
+    
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const port = process.env.PORT || 3000
 
+mongoose.connect('mongodb://localhost:27017/sample', {
+    useNewUrlParser: true,
+    user: 'mongoadmin',
+    pass: 'secret'
+})
+    .then(() => console.log('Connected successfully'))
+    .catch(() => console.log('Not connected'))
+
 app.get('/', (req, res) => {
     res.json({
-        message : 'Hello World Baby'
+        message: 'Hello World Baby'
     })
 })
 
 app.listen(port, () => {
-    console.log('App running on ' + port)
+    console.log('App running')
 })
